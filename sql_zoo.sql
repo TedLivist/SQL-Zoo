294c37e94dbe8b6bdc353aabb77ca002d17098fe
unknown <livistteddy@gmail.com>
2021-05-30 22:13:13 +0100

    Select name and population of few countries

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -1,4 +1,7 @@
 /* show the population of Germany */
-
 SELECT population FROM world
   WHERE name = 'Germany'
+
+/* Show the name and the population for 'Sweden', 'Norway' and 'Denmark'. */
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark') ORDER BY population DESC;
