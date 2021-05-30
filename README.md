e5b15d740bb41ba8909de4f5b42f22afbc45f72d
unknown <livistteddy@gmail.com>
2021-05-31 00:24:33 +0100

    Add answers for SELECT_from_Nobel and modify README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# SQL-Zoo
+## SQL-Zoo
+
+> This project is the provision of answers to the SQL Zoo Tutorials 0 - 9 Exercises
+
+#### The exercises can be found [here](http://sqlzoo.net/wiki/Main_Page)
+
+The `sql_zoo.sql` file is divided into the tutorial sub-headings:
+
+> SELECT basics
+
+> SELECT from World
+
+> SELECT from Nobel
--- sql_zoo.sql
+++ sql_zoo.sql
@@ -87,3 +87,39 @@ SELECT name
 WHERE name NOT LIKE '% %'
  AND name LIKE '%a%' AND name LIKE '%b%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%'
 
+
+-- SELECT from NOBEL
+-- Change the query shown so that it displays Nobel prizes for 1950.
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+
+-- Show who won the 1962 prize for Literature.
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+
+-- Show the year and subject that won 'Albert Einstein' his prize.
+SELECT yr, subject
+FROM nobel
+WHERE winner = 'Albert Einstein'
+
+-- Give the name of the 'Peace' winners since the year 2000, including 2000.
+SELECT winner
+FROM nobel
+WHERE yr >= 2000 AND subject = 'Peace'
+
+-- Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Literature'
+ AND yr BETWEEN 1980 AND 1989
+
+/* Show all details of the presidential winners:
+- Theodore Roosevelt
+- Woodrow Wilson
+- Jimmy Carter
+- Barack Obama */
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama')
