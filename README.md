563e143007c309fd8b92e7f16acb936f18d073fb
unknown <livistteddy@gmail.com>
2021-05-31 01:45:08 +0100

    Complete solution for 'SELECT from nobel' module

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > This project is the provision of answers to the SQL Zoo Tutorials 0 - 9 Exercises
 
-#### The exercises can be found [here](http://sqlzoo.net/wiki/Main_Page)
+The exercises can be found [here](http://sqlzoo.net/wiki/Main_Page)
 
 The `sql_zoo.sql` file is divided into the tutorial sub-headings:
 
--- sql_zoo.sql
+++ sql_zoo.sql
@@ -123,3 +123,53 @@ WHERE subject = 'Literature'
 - Barack Obama */
 SELECT * FROM nobel
  WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama')
+
+-- Show the winners with first name John
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'John%'
+--- Another answer ---
+SELECT winner
+FROM nobel
+WHERE LEFT(winner, 4) = 'John'
+
+-- Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
+SELECT *
+FROM nobel
+WHERE subject = 'Physics' AND yr = 1980 OR subject = 'Chemistry' AND yr = 1984
+
+-- Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
+SELECT *
+FROM nobel
+WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine')
+
+/* Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910)
+together with winners of a 'Literature' prize in a later year (after 2004, including 2004) */
+SELECT *
+FROM nobel
+WHERE subject = 'Medicine' AND yr < 1910
+ OR subject = 'Literature' AND yr >= 2004
+
+-- Find all details of the prize won by PETER GRÜNBERG
+-- N.B. 'umlaut' character: Alt + 0 + 2 + 2 + 0 = Ü; Alt + 0 + 2 + 5 + 2 = ü
+SELECT *
+FROM nobel
+WHERE winner = 'Peter Grünberg'
+
+-- Find all details of the prize won by EUGENE O'NEILL
+-- N.B. how to escape apostrophe
+SELECT *
+FROM nobel
+WHERE winner = 'Eugene O''Neill'
+
+-- List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner
+
+-- Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+SELECT winner, subject
+FROM nobel
+WHERE yr = 1984
+ORDER BY
+CASE WHEN subject IN ('Physics', 'Chemistry') THEN 1 ELSE 0 END, subject, winner
