5f9eb5c46a63a1a5b0f045866351e93e422ef4bd
unknown <livistteddy@gmail.com>
2021-06-01 14:16:58 +0100

    Create select_basics file

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -1,15 +1,4 @@
--- SELECT basics
--- Show the population of Germany
-SELECT population FROM world
-  WHERE name = 'Germany'
 
--- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
-SELECT name, population FROM world
-  WHERE name IN ('Sweden', 'Norway', 'Denmark') ORDER BY population DESC;
-
--- Show the country and the area for countries with an area between 200,000 and 250,000.
-SELECT name, area FROM world
-  WHERE area BETWEEN 200000 AND 250000
 
 
 -- SELECT from WORLD
